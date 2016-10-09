467a92c400a12e46eaed40d1197fe030e2d89b26
Hrvoje <hrvoje.hudoletnjak@gmail.com>
2016-10-09 23:53:17 +0100

    Added full dotnet project, wip

---
+++ JA.Pagination.MVC5/HtmlExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JA.Pagination.MVC5
+{
+    public static class HtmlExtension
+    {
+        public static IHtmlString RenderPager(this HtmlHelper helper, int currentPage, int totalPages, Func<int, string> urlBuilder)
+        {
+            return helper.Raw(Pager.Build(currentPage, totalPages, urlBuilder).Render());
+        }
+    }
+}
